b9b2dc46be7555ebb9506ba2bba577ab79f08b85
Derpmander <70146049+Derpmander@users.noreply.github.com>
2020-11-30 14:42:57 +0100

    5.2 klar, 5.4 på börjad

---
+++ 5.2.py
@@ -0,0 +1,11 @@
+t = str(input("Skriv en text: "))
+print(len(t))
+print(f"Det första tecknet är {t[0]}")
+h = len(t)
+h = h - 1
+print(f"Det sista tecknet är {t[h]}")
+
+if t[0]==t[h]:
+    print("Good job")
+elif t[0] != t[h]:
+    print("No")
