95e46c9b842224e995e47bf666ae87ee0ca9492f
Derpmander <70146049+Derpmander@users.noreply.github.com>
2021-01-25 15:05:47 +0100

    Update 5.7.5.3.py

--- 5.7.5.3.py
+++ 5.7.5.3.py
@@ -1,7 +1,7 @@
 persnum = input(str("Skriv in ditt personnummer (yyyymmdd-xxxx):"))
 print (persnum) 
 print (f"det tal är nr {persnum[11]}")
-if persnum[11] == 1:
+if persnum[11] == 1 or persnum[11] == 3 or persnum[11] == 5 or persnum[11] == 7 or persnum[11] == 9:
     print ("Woman")
 else:
     print ("Man")
